14a5e0cb29ce9640dea28e8966924d69cebb5d3c
Eidrian2005 <eserrano@fwdcostarica.com>
2024-08-11 17:40:42 -0600

    Tarea Terminada

---
+++ js/funciones.js
@@ -0,0 +1,47 @@
+const botonPopver = document.getElementById('botonPop')
+const popver = document.getElementById('popover')
+const botonAgregar = document.querySelectorAll('.botonAgregar')
+let carritoCompras = document.getElementById('carrito')
+let listaCompra = []
+
+botonPopver.addEventListener('click',function() {
+
+    if (popver.style.display === 'none' || popver.style.display === '') {
+        popver.style.display = 'block';
+    } else {
+        popver.style.display = 'none'
+    }
+})
+
+botonAgregar.forEach(boton => {
+    boton.addEventListener('click', function() {
+        const nombreProducto = this.previousElementSibling.textContent;
+
+        listaCompra.push(nombreProducto)
+        actualizarCarrito()
+    })
+})
+
+function actualizarCarrito() {
+    carritoCompras.innerHTML = ''
+
+    listaCompra.forEach(producto =>{
+        const objeto = document.createElement('p')
+        const botonElim = document.createElement('button')
+        botonElim.innerHTML = "eliminar"
+        objeto.textContent = producto
+        botonElim.classList.add("btnEliminar")
+        carritoCompras.appendChild(objeto)
+        carritoCompras.appendChild(botonElim)
+
+        botonElim.addEventListener("click", function(){
+
+            console.log("hay que quitar");
+            objeto.remove(this)
+            botonElim.remove(this)
+        })
+    })
+
+    
+}
+
